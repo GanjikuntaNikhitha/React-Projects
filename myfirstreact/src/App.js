2984a4e9394b44e8dcfe0027547a000c497fb12d
GanjikuntaNikhitha <nikhitha.ganjigunta62@gmail.com>
2021-03-06 20:32:34 +0530

    Project Setup

--- myfirstreact/src/App.js
+++ myfirstreact/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// import ReactDOM from 'react-dom';
+import './App.css'
+import Header from './Components/Header';
+import About from './Components/About';
+import Contact from './Components/Contact';
+import Services from './Components/Services'
 
 function App() {
   return (
+// now just we are writing all the components in app.js to display in webpage
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <About></About>
+      <Contact></Contact>
+      <Services></Services>
     </div>
-  );
+  )
 }
 
-export default App;
+  export default App;
